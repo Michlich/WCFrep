b8e8d316ecbbc0c52a8df738f631ccf9b6aedcc5
Michlich <150546066+Michlich@users.noreply.github.com>
2024-07-22 22:33:38 +0300

    Add files via upload

---
+++ WcfServiceLibrary2/IService1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+// Используйте контракт данных, как показано в примере ниже, чтобы добавить составные типы к операциям служб.
+
+
+
+namespace WcfServiceLibrary2
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени интерфейса "IService1" в коде и файле конфигурации.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        [WebGet]
+        string UploadFile(Stream fileContents);
+        [OperationContract]
+        [WebGet]
+        string TestFunction(string testcont);
+
+        // TODO: Добавьте здесь операции служб
+    }
+
+
+    // TODO: Добавьте здесь операции служб
+}
+
+// Используйте контракт данных, как показано в примере ниже, чтобы добавить составные типы к операциям служб.
+// В проект можно добавлять XSD-файлы. После построения проекта вы можете напрямую использовать в нем определенные типы данных с пространством имен "WcfServiceLibrary2.ContractType".
+
---
+++ WcfServiceLibrary2/Service1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using WcfServiceLibrary2;
+
+// ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Реструктуризация" можно использовать для одновременного изменения имени класса "Service" в коде, SVC-файле и файле конфигурации.
+namespace WcfServiceLibrary2
+{
+    public class Service1 : IService1
+    {
+        public string UploadFile(Stream fileContents)
+        {
+            byte[] buffer = new byte[1073741824];
+            int bytesRead, totalBytesRead = 0;
+            do
+            {
+                bytesRead = fileContents.Read(buffer, 0, buffer.Length);
+                totalBytesRead += bytesRead;
+            } while (bytesRead > 0);
+            return "Service: Received file with bytes" + totalBytesRead;
+        }
+        public string TestFunction(string testcont)
+        {
+            return testcont + "123";
+        }
+    }
+}
